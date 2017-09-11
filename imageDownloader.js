2580daa5f2325c9308172fa0168a5da327277cf9
Parmvir <parmvirsthind@gmail.com>
2017-09-11 20:21:37 +0000

    Created a js file that requests and downloads images from url

---
+++ imageDownloader.js
@@ -0,0 +1,21 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+
+        .on('error', function(err) {
+          throw err;
+        })
+
+        .on('response', function(response) {
+          console.log('Response Status Code: ', response.statusCode);
+          console.log('Response Status Message: ', response.statusMessage);
+          console.log('Response content type: ', response.headers['content-type']);
+          console.log("Downloading image...");
+        })
+
+        .pipe(fs.createWriteStream('./future.jpg'))
+
+        .on('end', function() {
+          console.log('Download complete');
+        });
